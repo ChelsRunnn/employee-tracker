c41ee0f9b1525a71e6cc6aeb755e5c06b2ae9b74
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-14 11:59:43 -0500

    primary/foreign keys set

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,28 @@ USE theRanch_db;
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 DROP TABLE IF EXISTS role;
 CREATE TABLE role (
-    id INT,
+    id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL, 
-    department_id INT
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    id INT, 
+    id INT NOT NULL PRIMARY KEY, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT, 
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL, 
     manager_id INT
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO department (id, name)
 VALUES (01, 'Admin'),
-       (02, 'Livestock'),
-       (03, 'Equine');
+       (02, 'Ranch'),
+       (03, 'Home');
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (101, 'Ranch Manager', 100000.00, 01),
        (102, 'Financial Advisor and Payroll', 80000.00, 01),
-       (201, 'Cowboy', 60000.00, 02),
-       (301, 'Wrangler', 60000.00, 03),
-       (202, 'Ranch Hand', 45000.00, 02);
+       (201, 'Wrangler', 60000.00, 02),
+       (202, 'Ranch Hand', 45000.00, 02),
+       (301, 'Cook', 50000.00, 03),
